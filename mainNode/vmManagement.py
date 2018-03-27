4faa5a0ecab42e534507cb9c5342eb892dfbd384
kashifqazi <kmq2@njit.edu>
2018-03-27 16:21:31 +0000

    main fn

--- mainNode/vmManagement.py
+++ mainNode/vmManagement.py
@@ -4,4 +4,14 @@ def startVM(id):
 def stopVM(id):
 	print id
 
+def migrateCont(contid, srcid, destid):
+	#use criu to migrate
 
+def manage():
+	for cont in containers:
+		#check predicted value
+		#check current VM capacity
+		#if capacity mismatch
+			startVM(destid)
+			migrateCont(cont, srcid, destid)
+			stopVM(srcid)
