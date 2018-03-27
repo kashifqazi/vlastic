322f3c18fd1f0511576a24b5f66188bf4cd26ed5
kashifqazi <kmq2@njit.edu>
2018-03-27 16:37:57 +0000

    start stop fns

--- mainNode/vmManagement.py
+++ mainNode/vmManagement.py
@@ -1,17 +1,28 @@
+import subprocess
+
 def startVM(id):
-	print id
+	print (id)
+	bashCommand = "aws ec2 start-instances --instance-ids " + str(id)
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
 
 def stopVM(id):
-	print id
+	print (id)
+	bashCommand = "aws ec2 stop-instances --instance-ids " + str(id)
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
 
 def migrateCont(contid, srcid, destid):
 	#use criu to migrate
-
+	print (contid)
 def manage():
-	for cont in containers:
+	#for cont in containers:
 		#check predicted value
 		#check current VM capacity
 		#if capacity mismatch
-			startVM(destid)
-			migrateCont(cont, srcid, destid)
-			stopVM(srcid)
+	#		startVM(destid)
+	#		migrateCont(cont, srcid, destid)
+	#		stopVM(srcid)
+	startVM("i-046d3a8ec1ca1a3fb")
+
+manage()
